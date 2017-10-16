4a9dc680178e791aa822d7dc20221417218d06e8
Evanchen6 <chen_chen311a@163.com>
2017-10-16 11:33:27 +0800

    modify gnss can't show cn screen issue with 2nd menu
    modify gnss can't show cn screen issue with 2nd menu

--- iot_sdk_demo/src/traing_type/traing_type_screen.c
+++ iot_sdk_demo/src/traing_type/traing_type_screen.c
@@ -125,6 +125,9 @@ void show_traing_type_screen(void);
 
 void traing_type_event_handler(message_id_enum event_id, int32_t param1, void* param2)
 {
+    if (curr_event_handler) {
+        curr_event_handler(event_id, param1, param2);
+    }
 
 }
 static void traing_type_screen_cntx_init()
